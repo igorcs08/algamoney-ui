c4115318fe7585dcb9845932e237fb92e59e5c58
Igor Cavalcante <igor.cavalcante96@gmail.com>
2022-01-27 17:43:13 -0300

    Implementa métodos para atualizar cadastro de pessoas

--- src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,8 +1,11 @@
-import { NgForm } from '@angular/forms';
-import { MessageService } from 'primeng/api';
-import { PessoaService } from './../service/pessoa.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { timeStamp } from 'console';
+import { MessageService } from 'primeng/api';
 import { Pessoa } from 'src/app/core/model';
+import { PessoaService } from './../service/pessoa.service';
 
 @Component({
   selector: 'app-pessoa-cadastro',
@@ -15,22 +18,74 @@ export class PessoaCadastroComponent implements OnInit {
 
   constructor(
     private pessoaService: PessoaService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private title: Title
   ) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Novo cadastro de pessoa');
+    const codigoPessoa = this.route.snapshot.params['codigo'];
+
+    if (codigoPessoa) this.carregarPessoa(codigoPessoa);
+
   }
 
-  salvar(pessoaForm: NgForm) {
+  get editando() {
+    return Boolean(this.pessoa.codigo);
+  }
+
+  carregarPessoa(codigo: number) {
+    this.pessoaService.buscarPorCodigo(codigo)
+    .subscribe(pessoa => {
+      this.pessoa = pessoa;
+      this.atualizarTituloEdicao();
+    });
+  }
+
+  salvar(form: NgForm) {
+    if (this.editando) {
+      this.atualizarPessoa(form);
+    } else {
+      this.adicionaPessoa(form);
+    }
+  }
+
+  adicionaPessoa(pessoaForm: NgForm) {
     this.pessoaService.adicionar(this.pessoa)
-    .subscribe(() => {
-      this.messageService.add({
+      .subscribe(() => {
+        this.messageService.add({
+          severity: 'success',
+          detail: 'Pessoa adicionanda com sucesso!'
+        });
+        pessoaForm.reset();
+        this.pessoa = new Pessoa();
+      })
+  }
+
+  atualizarPessoa(pessoaForm: NgForm) {
+    this.pessoaService.atualizar(this.pessoa)
+    .subscribe(pessoa => {
+      this.pessoa = pessoa;
+      this.messageService.add( {
         severity: 'success',
-        detail: 'Pessoa adicionanda com sucesso!'
+        detail: 'Dados alterados com sucesso!'
       });
-      pessoaForm.reset();
-      this.pessoa = new Pessoa();
+      this.atualizarTituloEdicao();
     })
+  }
+
+  novo(form: NgForm) {
+    form.reset();
+    setTimeout(() => {
+      this.pessoa = new Pessoa();
+    }, 1);
+    this.router.navigate(['/pessoas/novo']);
+  }
+
+  atualizarTituloEdicao() {
+    this.title.setTitle(`Edição dos dados de: ${this.pessoa.nome}`);
   }
 
 }
