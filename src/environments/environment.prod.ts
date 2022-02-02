018ab5782bd78dbb492ba16a2e0341b51295a06e
Igor Cavalcante <igor.cavalcante96@gmail.com>
2022-02-02 17:54:56 -0300

    Correção na URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-bend.herokuapp.com/',
+  apiUrl: 'https://algamoney-bend.herokuapp.com',
   tokenAllowedDomains: [ /localhost:8080/ ],
   tokenDisallowedRoutes: [/\/outh2\/token/],
   oauthCallbackUrl: 'https://algamoney-ui-gamma.vercel.app/authorized',
