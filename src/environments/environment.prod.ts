4bc0bec56aa210954eb63adc5edd2ac56a8a0e1e
Igor Cavalcante <igor.cavalcante96@gmail.com>
2022-02-02 17:33:03 -0300

    Fazendo deploy no VercelApp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://algamoney-bend.herokuapp.com/',
+  tokenAllowedDomains: [ /localhost:8080/ ],
   tokenDisallowedRoutes: [/\/outh2\/token/],
-  oauthCallbackUrl: 'https://oidcdebugger.com/debug'
+  oauthCallbackUrl: 'https://oidcdebugger.com/debug',
+  logoutRedirectToUrl: 'http://local-algamoney.com:8000'
 };
